d0a313630f412f47ad42f8598fe338cf2c680959
Rio Ikeda <examinare000@gmail.com>
2025-10-03 01:13:06 +0900

    修正: Position型のimportを追加してコンパイルエラーを解消

--- tests/unit/integration/wikilink-keybinding.test.ts
+++ tests/unit/integration/wikilink-keybinding.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
 import * as vscode from 'vscode';
+import { Position } from 'vscode';
 import { WikiLinkContextProvider } from '../../../src/providers/WikiLinkContextProvider';
 
 interface MockExtensionContext {
